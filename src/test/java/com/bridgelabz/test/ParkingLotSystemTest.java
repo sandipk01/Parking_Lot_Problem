efb9d43d8f00f69f1bb6447fd013e5380daeae72
sandipk01 <sandipkengar01@gmail.com>
2020-04-16 11:36:43 +0530

    ADDED : Test case for when parking space is full should throw no parking space exception

--- src/test/java/com/bridgelabz/test/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/test/ParkingLotSystemTest.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.test;
 
+import com.bridgelabz.code.exception.ParkingLotException;
 import com.bridgelabz.code.service.ParkingLotSystem;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,20 +17,20 @@ public class ParkingLotSystemTest {
     }
 
     @Test
-    public void givenVehicle_WhenPark_ShouldReturnTrue() {
+    public void givenVehicle_WhenPark_ShouldReturnTrue() throws ParkingLotException {
         boolean isPark = parkingLotSystem.park(vehicle);
         Assert.assertEquals(true, isPark);
     }
 
     @Test
-    public void givenVehicle_WhenAlreadyPark_ShouldReturnFalse() {
+    public void givenVehicle_WhenAlreadyPark_ShouldReturnFalse() throws ParkingLotException {
         parkingLotSystem.park(vehicle);
-        boolean isPark = parkingLotSystem.park(new Object());
+        boolean isPark = parkingLotSystem.isVehicleParked(new Object());
         Assert.assertEquals(false, isPark);
     }
 
     @Test
-    public void givenVehicle_WhenUnPark_ShouldReturnTrue() {
+    public void givenVehicle_WhenUnPark_ShouldReturnTrue() throws ParkingLotException {
         parkingLotSystem.park(vehicle);
         boolean isPark = parkingLotSystem.unPark(vehicle);
         Assert.assertEquals(true, isPark);
@@ -42,10 +43,20 @@ public class ParkingLotSystemTest {
     }
 
     @Test
-    public void givenVehicle_WhenTryToUnParkDifferentVehicle_ShouldReturnFalse() {
+    public void givenVehicle_WhenTryToUnParkDifferentVehicle_ShouldReturnFalse() throws ParkingLotException {
         parkingLotSystem.park(vehicle);
         boolean isPark = parkingLotSystem.unPark(new Object());
         Assert.assertEquals(false, isPark);
+    }
+
+    @Test
+    public void givenVehicleTryPark_WhenParkingSpaceIsFull_ShouldReturnThrowException()  {
+        try {
+            parkingLotSystem.park(vehicle);
+            parkingLotSystem.park(new Object());
+        } catch (ParkingLotException e) {
+            Assert.assertEquals(ParkingLotException.TypeOfException.NO_PARKING_SPACE_EXCEPTION, e.type);
+        }
     }
 
 }
