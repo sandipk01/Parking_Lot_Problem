fb7721a35961d73502f64a8112cf68529b17f96d
sandipk01 <sandipkengar01@gmail.com>
2020-04-16 11:35:07 +0530

    REFACTORED : Parking space is not available excption is added

--- src/main/java/com/bridgelabz/code/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/code/service/ParkingLotSystem.java
@@ -1,17 +1,25 @@
 package com.bridgelabz.code.service;
 
+import com.bridgelabz.code.exception.ParkingLotException;
+
 public class ParkingLotSystem {
 
     private Object vehicle;
 
     //Method for park a vehicle
-    public boolean park(Object vehicleObject) {
+    public boolean park(Object vehicleObject) throws ParkingLotException {
         if (this.vehicle != null)
-            return false;
+            throw new ParkingLotException("parking space is not available!",ParkingLotException.TypeOfException.NO_PARKING_SPACE_EXCEPTION);
         this.vehicle = vehicleObject;
         return true;
     }
 
+    //Check vehicle is parked or not
+    public boolean isVehicleParked(Object vehicleObject){
+        return (this.vehicle.equals(vehicleObject)) ? true : false;
+    }
+
+    //Vehicle for un Park
     public boolean unPark(Object vehicleObject) {
         if (this.vehicle == null) return false;
         if(this.vehicle==vehicleObject){
