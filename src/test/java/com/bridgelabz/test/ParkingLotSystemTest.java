2b6a9d87ed99e380d691819a0fb7859be01c99f5
sandipk01 <sandipkengar01@gmail.com>
2020-04-16 10:37:36 +0530

    ADDED : Test case if vehicle is park then should return true

--- src/test/java/com/bridgelabz/test/ParkingLotSystemTest.java
+++ src/test/java/com/bridgelabz/test/ParkingLotSystemTest.java
@@ -1,4 +1,23 @@
 package com.bridgelabz.test;
 
+import com.bridgelabz.code.ParkingLotSystem;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ParkingLotSystemTest {
+    private Object vehicle;
+    private ParkingLotSystem parkingLotSystem;
+
+    @Before
+    public void setUp() {
+        vehicle = new Object();
+        parkingLotSystem = new ParkingLotSystem();
+    }
+
+    @Test
+    public void givenVehicle_WhenPark_ShouldReturnTrue() {
+        boolean isPark = parkingLotSystem.park(vehicle);
+        Assert.assertEquals(true, isPark);
+    }
 }
