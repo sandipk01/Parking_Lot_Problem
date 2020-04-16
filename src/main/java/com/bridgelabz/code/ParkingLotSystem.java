3184095f15ffa68cae5956f6044f444c4b739cc1
sandipk01 <sandipkengar01@gmail.com>
2020-04-16 10:29:09 +0530

    ADDED : Initial commit

---
+++ src/main/java/com/bridgelabz/code/ParkingLotSystem.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.code;
+
+public class ParkingLotSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to parking lot system");
+    }
+
+}
