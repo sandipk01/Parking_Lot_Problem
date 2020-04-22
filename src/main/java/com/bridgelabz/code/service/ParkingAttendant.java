02c9acc8ce51580a15c8b14f57f0b79bc640f22f
sandipk01 <sandipkengar01@gmail.com>
2020-04-23 00:42:04 +0530

    REFACTORED : Method to find nearest free space

--- src/main/java/com/bridgelabz/code/service/ParkingAttendant.java
+++ src/main/java/com/bridgelabz/code/service/ParkingAttendant.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.code.service;
 
+import com.bridgelabz.code.exception.ParkingLotException;
 import com.bridgelabz.code.model.Vehicle;
 
 import java.util.Map;
@@ -7,9 +8,35 @@ import java.util.Map;
 public class ParkingAttendant extends Driver {
 
     private static int section = 65;
+    private ParkingLotSystem parkingLotSystem;
 
     public ParkingAttendant(ParkingLotSystem parkingLotSystem) {
         super(parkingLotSystem);
+        this.parkingLotSystem = parkingLotSystem;
+    }
+
+    @Override
+    public Map<String, Vehicle> parkAVehicle(Vehicle vehicle, Map<String, Vehicle> parkingLot) throws ParkingLotException {
+        if (parkingLotSystem.isParkingLotFull()) {
+            throw new ParkingLotException("No Space Available :", ParkingLotException.TypeOfException.NO_PARKING_SPACE);
+        } else if (parkingLotSystem.isVehicleParked(vehicle)) {
+            throw new ParkingLotException("Car is Already Parked :", ParkingLotException.TypeOfException.ALREADY_PARKED);
+        } else {
+            String key = nearestFreeSpace(parkingLot);
+            parkingLot.put(key, vehicle);
+            parkingLotSystem.isParkingLotFull();
+            parkingLotSystem.notifyObservers();
+        }
+        return parkingLot;
+    }
+
+    public static String nearestFreeSpace(Map<String,Vehicle> parkingLot){
+        for (Map.Entry<String, Vehicle> entry : parkingLot.entrySet()) {
+            if (entry.getValue()==null) {
+                return entry.getKey();
+            }
+        }
+        return null;
     }
 
     //Evenly distributed parking lots.
