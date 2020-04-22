850087dcd0942d3c91160ce3cead1bace68f3dde
sandipk01 <sandipkengar01@gmail.com>
2020-04-22 14:50:28 +0530

    ADDED : Getting current time method

--- src/main/java/com/bridgelabz/code/utils/Utils.java
+++ src/main/java/com/bridgelabz/code/utils/Utils.java
@@ -1,4 +1,13 @@
 package com.bridgelabz.code.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Utils {
+
+    public static String getCurrentTime() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(cal.getTime());
+    }
 }
