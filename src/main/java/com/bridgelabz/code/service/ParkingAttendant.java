578dadff74631ffed874e45025718e6f95564db6
sandipk01 <sandipkengar01@gmail.com>
2020-04-22 20:04:50 +0530

    ADDED : Implemented evenly distributed parking

--- src/main/java/com/bridgelabz/code/service/ParkingAttendant.java
+++ src/main/java/com/bridgelabz/code/service/ParkingAttendant.java
@@ -1,9 +1,29 @@
 package com.bridgelabz.code.service;
 
-public class ParkingAttendant extends Driver{
+import com.bridgelabz.code.model.Vehicle;
+
+import java.util.Map;
+
+public class ParkingAttendant extends Driver {
+
+    private static int section = 65;
 
     public ParkingAttendant(ParkingLotSystem parkingLotSystem) {
         super(parkingLotSystem);
     }
 
+    //Evenly distributed parking lots.
+    public static String evenlyDistributeLot(Map<String, Vehicle> parkingLot) {
+        int limit = 65 + ParkingLotSystem.getNoOfParkingLots();
+        for (Map.Entry<String, Vehicle> entry : parkingLot.entrySet()) {
+            if (entry.getKey().contains((char) section + " ") && entry.getValue() == null) {
+                section++;
+                return entry.getKey();
+            }
+            if (section >= limit) {
+                section = 65;
+            }
+        }
+        return null;
+    }
 }
