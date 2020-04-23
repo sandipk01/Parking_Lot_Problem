7b9f7b0e90c0548f8386985c2d03ede4670dfb46
sandipk01 <sandipkengar01@gmail.com>
2020-04-23 14:58:41 +0530

    REFACTORED : Method for getting max free space lot

--- src/main/java/com/bridgelabz/code/service/ParkingAttendant.java
+++ src/main/java/com/bridgelabz/code/service/ParkingAttendant.java
@@ -5,17 +5,15 @@ import com.bridgelabz.code.model.Vehicle;
 
 import java.util.Map;
 
-public class ParkingAttendant extends Driver {
+public class ParkingAttendant {
 
-    private static int section = 65;
     private ParkingLotSystem parkingLotSystem;
+    private int maxSection = 0;
 
     public ParkingAttendant(ParkingLotSystem parkingLotSystem) {
-        super(parkingLotSystem);
         this.parkingLotSystem = parkingLotSystem;
     }
 
-    @Override
     public Map<String, Vehicle> parkAVehicle(Vehicle vehicle, Map<String, Vehicle> parkingLot) throws ParkingLotException {
         if (parkingLotSystem.isParkingLotFull()) {
             throw new ParkingLotException("No Space Available :", ParkingLotException.TypeOfException.NO_PARKING_SPACE);
@@ -24,15 +22,13 @@ public class ParkingAttendant extends Driver {
         } else {
             String key = nearestFreeSpace(parkingLot);
             parkingLot.put(key, vehicle);
-            parkingLotSystem.isParkingLotFull();
-            parkingLotSystem.notifyObservers();
         }
         return parkingLot;
     }
 
-    public static String nearestFreeSpace(Map<String,Vehicle> parkingLot){
+    public String nearestFreeSpace(Map<String, Vehicle> parkingLot) {
         for (Map.Entry<String, Vehicle> entry : parkingLot.entrySet()) {
-            if (entry.getValue()==null) {
+            if (entry.getValue() == null) {
                 return entry.getKey();
             }
         }
@@ -40,17 +36,59 @@ public class ParkingAttendant extends Driver {
     }
 
     //Evenly distributed parking lots.
-    public static String evenlyDistributeLot(Map<String, Vehicle> parkingLot) {
-        int limit = 65 + ParkingLotSystem.getNoOfParkingLots();
+    public String evenlyDistributeLot(Map<String, Vehicle> parkingLot) {
         for (Map.Entry<String, Vehicle> entry : parkingLot.entrySet()) {
-            if (entry.getKey().contains((char) section + " ") && entry.getValue() == null) {
-                section++;
+            if (entry.getKey().contains(maxLotSpace(parkingLot)) && entry.getValue() == null) {
                 return entry.getKey();
-            }
-            if (section >= limit) {
-                section = 65;
             }
         }
         return null;
     }
+
+    //Get lot which has Maximum number of free space
+    public String getMaxFreeSpaceLot(Map<String, Vehicle> parkingLot) {
+        for (Map.Entry<String, Vehicle> entry : parkingLot.entrySet()) {
+            if (entry.getKey().contains(maxLotSpace(parkingLot)) && entry.getValue() == null) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    //Get lot which has Maximum number of free space
+    public String maxLotSpace(Map<String, Vehicle> parkingLot) {
+        int section = 65, counter = 0, maxCounter = 0, sectionLimit = 65 + ParkingLotSystem.getNoOfParkingLots();
+        if (iSLotEmpty((char) section + " ", parkingLot)) {
+            maxSection = section;
+            section++;
+        } else {
+            while (section <= sectionLimit) {
+                for (Map.Entry<String, Vehicle> entry : parkingLot.entrySet()) {
+                    if (entry.getKey().contains((char) section + " ") && entry.getValue() == null) {
+                        counter++;
+                    }
+                }
+                if (maxCounter < counter) {
+                    maxCounter = counter;
+                    maxSection = section;
+                }
+                counter = 0;
+                section++;
+            }
+        }
+        return (char) maxSection + " ";
+    }
+
+    //Get Lot which is completely empty
+    public boolean iSLotEmpty(String lot, Map<String, Vehicle> parkingLot) {
+        int counter = 0;
+        for (Map.Entry<String, Vehicle> entry : parkingLot.entrySet()) {
+            if (entry.getKey().contains(lot) && entry.getValue() == null)
+                counter++;
+        }
+        if (counter >= ParkingLotSystem.PARKING_LOT_SIZE)
+            return true;
+        return false;
+    }
+
 }
