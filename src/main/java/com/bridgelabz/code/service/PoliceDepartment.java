eb4f6f7cfa7900118c6118ed7d0b4e72c8fbe54a
sandipk01 <sandipkengar01@gmail.com>
2020-04-24 00:53:57 +0530

    REFACTORED : Search by color and brand name method is added

--- src/main/java/com/bridgelabz/code/service/PoliceDepartment.java
+++ src/main/java/com/bridgelabz/code/service/PoliceDepartment.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.code.service;
 
 import com.bridgelabz.code.enums.VehicleInquiry;
+import com.bridgelabz.code.model.ParkingDetails;
 import com.bridgelabz.code.model.Vehicle;
 
 import java.util.ArrayList;
@@ -9,16 +10,29 @@ import java.util.Map;
 
 public class PoliceDepartment {
 
-    private List<Vehicle> vehicles;
+    private List<Object> vehicles;
+    private ParkingLotSystem parkingLotSystem;
 
-    public List<Vehicle> getParkingDetails(VehicleInquiry vehicleInquiry, Map<String, Vehicle> parkingLot, String inquiry) {
+    public PoliceDepartment(ParkingLotSystem parkingLotSystem) {
+        this.parkingLotSystem = parkingLotSystem;
+    }
+
+    public List<Object> getParkingDetails(VehicleInquiry vehicleInquiry, Map<String, Vehicle> parkingLot, Map<Vehicle, ParkingDetails> parkingDetailsMap, String... inquiry) {
 
         switch (vehicleInquiry) {
             case COLOR:
                 vehicles = new ArrayList<>();
                 for (Vehicle vehicle : parkingLot.values()) {
-                    if (vehicle!=null && vehicle.getVehicleColor().equals(inquiry)) {
+                    if (vehicle != null && vehicle.getVehicleColor().equals(inquiry[0])) {
                         vehicles.add(vehicle);
+                    }
+                }
+                break;
+            case COLOR_AND_BRAND:
+                vehicles = new ArrayList<>();
+                for (Vehicle vehicle : parkingLot.values()) {
+                    if (vehicle != null && vehicle.getVehicleColor().equals(inquiry[0]) && vehicle.getBrandName().equals(inquiry[1])) {
+                        vehicles.add(parkingDetailsMap.get(vehicle));
                     }
                 }
                 break;
