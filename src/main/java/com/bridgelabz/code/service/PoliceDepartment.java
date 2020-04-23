fa18af1a6cc534e8e6c9e486d0b04845bab96c64
sandipk01 <sandipkengar01@gmail.com>
2020-04-23 21:57:36 +0530

    REFACTORED : Get parking details method is implemented

--- src/main/java/com/bridgelabz/code/service/PoliceDepartment.java
+++ src/main/java/com/bridgelabz/code/service/PoliceDepartment.java
@@ -1,4 +1,29 @@
 package com.bridgelabz.code.service;
 
+import com.bridgelabz.code.enums.VehicleInquiry;
+import com.bridgelabz.code.model.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class PoliceDepartment {
+
+    private List<Vehicle> vehicles;
+
+    public List<Vehicle> getParkingDetails(VehicleInquiry vehicleInquiry, Map<String, Vehicle> parkingLot, String inquiry) {
+
+        switch (vehicleInquiry) {
+            case COLOR:
+                vehicles = new ArrayList<>();
+                for (Vehicle vehicle : parkingLot.values()) {
+                    if (vehicle!=null && vehicle.getVehicleColor().equals(inquiry)) {
+                        vehicles.add(vehicle);
+                    }
+                }
+                break;
+        }
+        return vehicles;
+    }
+
 }
