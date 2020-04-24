c7bb6185712afbef4bbbf2e6472ae7221e96e283
sandipk01 <sandipkengar01@gmail.com>
2020-04-24 09:35:35 +0530

    REFACTORED : calculating time difference

--- src/main/java/com/bridgelabz/code/service/PoliceDepartment.java
+++ src/main/java/com/bridgelabz/code/service/PoliceDepartment.java
@@ -3,7 +3,12 @@ package com.bridgelabz.code.service;
 import com.bridgelabz.code.enums.VehicleInquiry;
 import com.bridgelabz.code.model.ParkingDetails;
 import com.bridgelabz.code.model.Vehicle;
+import com.bridgelabz.code.utils.Utils;
 
+import javax.rmi.CORBA.Util;
+import java.text.ParseException;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -12,17 +17,13 @@ public class PoliceDepartment {
 
     private List<Object> vehicles;
     private ParkingLotSystem parkingLotSystem;
-    private boolean isSecurityIncrease;
 
     public PoliceDepartment(ParkingLotSystem parkingLotSystem) {
         this.parkingLotSystem = parkingLotSystem;
     }
 
-    public boolean isSecurityIncrease() {
-        return isSecurityIncrease;
-    }
 
-    public List<Object> getParkingDetails(VehicleInquiry vehicleInquiry, Map<String, Vehicle> parkingLot, Map<Vehicle, ParkingDetails> parkingDetailsMap, String... inquiry) {
+    public List<Object> getParkingDetails(VehicleInquiry vehicleInquiry, Map<String, Vehicle> parkingLot, Map<Vehicle, ParkingDetails> parkingDetailsMap, String... inquiry) throws ParseException {
 
         switch (vehicleInquiry) {
             case COLOR:
@@ -50,6 +51,15 @@ public class PoliceDepartment {
                     }
                 }
                 break;
+            case TIME:
+                vehicles = new ArrayList<>();
+                for (Map.Entry<Vehicle, ParkingDetails> entry : parkingDetailsMap.entrySet()) {
+                    String currentTime = Utils.getIncreaseTime(Integer.parseInt(inquiry[0]),Integer.parseInt(inquiry[1]),Integer.parseInt(inquiry[2]));
+                    String parkingTime = entry.getValue().getParkTime();
+                    if (Utils.getDifferenceInMinutes(parkingTime,currentTime) >= Integer.parseInt(inquiry[2]) && Utils.getDifferenceInMinutes(parkingTime, currentTime) >= 0) {
+                        vehicles.add(entry.getKey());
+                    }
+                }
         }
         return vehicles;
     }
